0fc6455be2da71fd6dc076be13f24d6129f5797b
Pawel Krefta <pawel.krefta@gmail.com>
2015-04-29 13:38:11 +0200

    Updates

--- aliases.zsh
+++ aliases.zsh
@@ -116,3 +116,5 @@ done
 alias stfu="osascript -e 'set volume output muted true'"
 alias pumpitup="osascript -e 'set volume 7'"
 alias hax="growlnotify -a 'Activity Monitor' 'System error' -m 'WTF R U DOIN'"
+
+alias redis-start="redis-server /usr/local/etc/redis.conf"
